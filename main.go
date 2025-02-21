778889397a9c36010a6969e0a7a6d672f9149c00
Cody B Duncan <codybstrange93@gmail.com>
2025-02-22 10:53:19 +1100

    Refactor REPL code into repl.go

--- main.go
+++ main.go
@@ -1,25 +1,6 @@
 package main
 
-import (
-  "fmt"
-  "bufio"
-  "strings"
-  "os"
-)
-
 func main() {
-  scanner := bufio.NewScanner(os.Stdin)
-  for ; ; {
-    fmt.Printf("Pokedex > ")
-    ok := scanner.Scan()
-    if !ok {
-      fmt.Printf("Something went wrong with the scanning")
-    }
-    text := scanner.Text()
-    output := strings.ToLower(text)
-	  words := strings.Fields(output)
-	  fmt.Printf("Your command was: %s\n", words[0]) 
-  }
+  StartRepl()
 }
-
 
--- repl.go
+++ repl.go
@@ -2,8 +2,30 @@ package main
 
 import (
   "strings"
+  "bufio"
+  "os"
+  "fmt"
 )
 
+func StartRepl() {
+  reader := bufio.NewScanner(os.Stdin)
+  for ; ; {
+    fmt.Printf("Pokedex > ")
+    ok := reader.Scan()
+    if !ok {
+      fmt.Printf("Something went wrong with the scanning")
+    }
+    words := cleanInput(reader.Text())
+    if len(words) == 0 {
+      continue
+    }
+    
+    commandName := words[0]
+
+    //fmt.Printf("Your command was: %s\n", words[0]) 
+
+  }
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
